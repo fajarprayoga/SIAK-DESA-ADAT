1899ba097bd495ccb572d6dc256fc2a528164cb0
fajarprayoga <fajarprayoga23@gmail.com>
2023-08-19 16:46:41 +0800

    fix incomestatement

--- app/LedgerDetail.php
+++ app/LedgerDetail.php
@@ -4,20 +4,18 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class LedgerDetail extends Model
+class IncomeStatement extends Model
 {
-    protected $table='ledger_details';
+    protected $table = 'income_statement'; // Menggunakan huruf kecil dan garis bawah alih-alih camel case
     protected $fillable = [
-        'date',
-        'ledger_id',
-        'account_id',
-        'types',
-        'amount',
-        'description'
+        'register',
+        'title',
+        'status',
+        'note'
     ];
 
-    public function account()
+    public function incomeStatementDetail() // Menggunakan gaya camel case untuk nama metode
     {
-        return $this->belongsTo('App\Account',  'account_id');
+        return $this->hasMany(Incomestatement_detail::class, 'income_statement_id', 'id'); // Gunakan ::class untuk namespace
     }
 }
