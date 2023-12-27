be8ef9989e70702adcbf32c34acc0b6be6e33518
fajarprayoga <fajarprayoga23@gmail.com>
2023-12-27 13:36:28 +0800

    pamabayaran karyawan

--- resources/views/accounting/incomestatement/create.blade.php
+++ resources/views/accounting/incomestatement/create.blade.php
@@ -7,29 +7,29 @@
             <hr>
             <div class="card">
                 <div class="card-body">
-                   <form action="{{ route('accounting.incomestatement.store') }}" method="POST">
-                    @csrf
+                    <form action="{{ route('accounting.incomestatement.store') }}" method="POST">
+                        @csrf
                         <div class="mb-3">
                             <label for="">Pilih Neraca Saldo </label>
-                            <select  name="trial_balance_id" class="select2 w-100"  required>
+                            <select name="trial_balance_id" class="select2 w-100" required>
                                 <option value="">-- pilih --</option>
                                 @foreach ($trial_balances as $trial_balance)
-                                    <option value="{{$trial_balance->id}}">{{$trial_balance->title}}</option>
+                                    <option value="{{ $trial_balance->id }}">{{ $trial_balance->title }}</option>
                                 @endforeach
                             </select>
                         </div>
                         <div class="mb-3">
                             <label for="register" class="form-label">@lang('global.incomestatement.register')</label>
-                            <input class="form-control" type="text" id="date" name="register" required >
+                            <input class="form-control" type="text" id="date" name="register" required>
                         </div>
                         <div class="mb-3">
                             <label for="title" class="form-label">@lang('global.incomestatement.title')</label>
-                            <input class="form-control" type="text" id="date" name="title" required >
+                            <input class="form-control" type="text" id="date" name="title" required>
                         </div>
-                        <div class="mb-3">
+                        {{-- <div class="mb-3">
                             <label for="title" class="form-label">Potongan  Biaya Karyawan</label>
                             <input class="form-control price" type="text" id="piece_sand_super" name="amount[]" >
-                        </div>
+                        </div> --}}
                         {{-- <div class="mb-3">
                             <label for="piece_sand_super" class="form-label">@lang('global.incomestatement.piece_sand_super')</label>
                             <input class="form-control price" type="text" id="piece_sand_super" name="amount[]" >
@@ -47,7 +47,7 @@
                             <input class="form-control price" type="text" id="sale_freight_price" name="amount[]" >
                         </div> --}}
                         <button class="btn btn-primary" type="submit">Tambah</button>
-                   </form>
+                    </form>
                 </div>
             </div>
         </div>
@@ -58,32 +58,31 @@
 @section('script')
     <script type="text/javascript">
         // $(document).ready(() =>{
-            $('#date').bootstrapMaterialDatePicker({
-                format : 'DD-MM-YYYY',
-                time:false,
-                monthPicker:true,
-                year:true
-            });
+        $('#date').bootstrapMaterialDatePicker({
+            format: 'DD-MM-YYYY',
+            time: false,
+            monthPicker: true,
+            year: true
+        });
 
 
-            $(document).on('keyup','.price',function(){
-                // getTotal();
-                // alert('jalo')
-                formatRp()
-            });
+        $(document).on('keyup', '.price', function() {
+            // getTotal();
+            // alert('jalo')
+            formatRp()
+        });
 
 
-            function formatRp () {
-                var amount = $(".price").map(function(index, value){
-                    var rupiah = parseInt($(value).val() == ''? 0:$(value).val().split('.').join(""))
-                     $(value).val(function(index, item) {
-                        return item
+        function formatRp() {
+            var amount = $(".price").map(function(index, value) {
+                var rupiah = parseInt($(value).val() == '' ? 0 : $(value).val().split('.').join(""))
+                $(value).val(function(index, item) {
+                    return item
                         .replace(/\D/g, "")
-                        .replace(/\B(?=(\d{3})+(?!\d))/g, ".")
-                        ;
-                    });
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ".");
                 });
-            }
+            });
+        }
         // });
     </script>
 @endsection
